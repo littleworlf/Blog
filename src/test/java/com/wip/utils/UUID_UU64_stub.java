9db9642ed2354b47accd9d3a67b6fc7237cd6ba1
littleha233 <2722783643@qq.com>
2019-07-02 16:19:20 +0800

    Add files via upload

--- src/test/java/com/wip/utils/UUID_UU64_stub.java
+++ src/test/java/com/wip/utils/UUID_UU64_stub.java
@@ -1,1 +1,65 @@
+package com.wip.utils;
 
+import static com.wip.utils.UUID.UU32;
+import static com.wip.utils.UUID.UU64;
+
+public class UUID_UU64_stub {
+
+
+    private static final char[] _UU64 = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private static final char[] _UU32 = "0123456789abcdefghijklmnopqrstuv".toCharArray();
+
+
+    public boolean test(Object uu){
+        if(uu==null){
+            System.out.println("参数为空");
+            return false;
+        }else if(!(uu instanceof java.util.UUID)){
+            System.out.println("参数类型错误");
+            return false;
+        }
+        return true;
+    }
+
+    public static String TestUU64() {
+        return UU64(java.util.UUID.randomUUID());
+    }
+
+    public boolean UT_TC_02_02_01(){
+
+        boolean successful = false;
+        if(!test(null)){
+            return false;
+        }
+        return true;
+    }
+
+
+    public boolean UT_TC_02_02_02(){
+        if(!test("dwad--ad!+=adawd")){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        UUID_UU64_stub test =new UUID_UU64_stub();
+        System.out.println(test.UT_TC_02_02_04());
+    }
+
+    public boolean UT_TC_02_02_03(){
+        if(!test(UU32())){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean UT_TC_02_02_04(){
+        if(!test(TestUU64())){
+            return false;
+        }
+        return  true;
+    }
+
+
+}
---
+++ src/test/java/com/wip/utils/UUID_random_dirver.java
@@ -0,0 +1,26 @@
+package com.wip.utils;
+
+public class UUID_random_dirver {
+
+
+
+    public static void main(String[] args) {
+        UUID_random_stub test = new UUID_random_stub();
+
+        System.out.println("Testcase1'result:"+test.UT_TC_02_01_01());
+        System.out.println("Testcase2'result:"+test.UT_TC_02_01_02());
+        System.out.println("Testcase3'result:"+test.UT_TC_02_01_03());
+        System.out.println("Testcase4'result:"+test.UT_TC_02_01_04());
+        System.out.println("Testcase5'result:"+test.UT_TC_02_01_05());
+        System.out.println("Testcase6'result:"+test.UT_TC_02_01_06());
+        System.out.println("Testcase7'result:"+test.UT_TC_02_01_07());
+
+
+
+
+    }
+
+
+
+
+}
